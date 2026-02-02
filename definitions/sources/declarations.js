77283446695f7fdb781c931e355117391804e0a2
Ismet Books <ismat.books@gmail.com>
2026-02-02 20:06:19 +0000

    sample ga4 data

---
+++ definitions/sources/declarations.js
@@ -0,0 +1,13 @@
+declare({
+    type: "declaration",
+    database: "codezone-465722",
+    schema: "analytics_480513919",
+    name: "ga4_sample_data"
+});
+
+declare({
+    type: "declaration",
+    database: "codezone-465722",
+    schema: "analytics_480513919",
+    name: "events_*"
+})
---
+++ includes/helpers.js
@@ -0,0 +1,3 @@
+module.exports = {
+    start_date: "2025-12-01"
+}
